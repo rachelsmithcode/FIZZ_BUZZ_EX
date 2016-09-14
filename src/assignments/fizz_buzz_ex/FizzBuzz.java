be8bd8dc810a9ae72fa9abd487a5a19f6e358f35
rachelsmithcode <Rachelcodesmith@outlook.com>
2016-09-14 20:07:13 +0100

    slight refactor, checkMultiple method

--- src/assignments/fizz_buzz_ex/FizzBuzz.java
+++ src/assignments/fizz_buzz_ex/FizzBuzz.java
@@ -6,12 +6,16 @@ package assignments.fizz_buzz_ex;
 public class FizzBuzz {
 
     public void enterNumber(int i) {
-        if (i % 3 == 0) {
+        if (checkMultiple(i, 3)) {
             System.out.println("Fizz");
-        } else if (i % 5 == 0) {
+        } else if (checkMultiple(i, 5)) {
             System.out.println("Buzz");
         } else {
             System.out.println(i);
         }
+    }
+
+    private boolean checkMultiple(int i, int n) {
+        return i % n == 0;
     }
 }
